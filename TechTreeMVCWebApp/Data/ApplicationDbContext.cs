6638f92d248089e2fa550a3190e7cce3f6268334
Ray Ojuka <ray@pathwaysinternational.com>
2022-06-30 22:27:31 +0300

    Add project files.

---
+++ TechTreeMVCWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TechTreeMVCWebApp.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [StringLength(250)]
+        public string FirstName { get; set; }
+        [StringLength(250)]
+        public string SecondName { get; set; }
+        [StringLength(250)]
+        public string Address1 { get; set; }
+        [StringLength(250)]
+        public string Address2 { get; set; }
+        [StringLength(50)]
+        public string PostCode { get; set; }
+    }
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
